bb05f2f823c7534f897faa5807387ebb51a17ff8
mohamedhany72 <medocapo304@yahoo.com>
2024-06-06 17:04:30 +0300

    feat: add form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,23 +6,47 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({ invalid_type_error: 'Please select a customer.' }),
   customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
   const rawFormData = {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   };
   //   const rawFormData = Object.fromEntries(formData.entries());
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse(rawFormData);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   try {
@@ -30,23 +54,39 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch (error) {
-    console.log(error);
-    throw new Error('Failed to Create Invoice');
+  } catch (Error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
   const rawFormData = {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   };
   //   const rawFormData = Object.fromEntries(formData.entries());
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse(rawFormData);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   //   const date = new Date().toISOString().split('T')[0];
   try {
@@ -55,9 +95,10 @@ export async function updateInvoice(id: string, formData: FormData) {
     SET customer_id=${customerId}, amount=${amountInCents}, status=${status} 
     WHERE id=${id}
   `;
-  } catch (error) {
-    console.log(error);
-    throw new Error('Failed to Update Invoice');
+  } catch (Error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
@@ -72,9 +113,10 @@ export async function deleteInvoice(id: string) {
     DELETE FROM invoices
     WHERE id=${id}
   `;
-  } catch (error) {
-    console.log(error);
-    throw new Error('Failed to Delete Invoice');
+  } catch (Error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
