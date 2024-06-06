709c5f8b955db974ffdeb9c80146f9f3d7825481
mohamedhany72 <medocapo304@yahoo.com>
2024-06-06 18:13:28 +0300

    feat: add metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,13 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+  description: 'The invoices page for Acme Dashboard',
+};
+
 export default async function Page({
   searchParams,
 }: {
